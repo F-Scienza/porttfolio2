bc45a6796992099544722c2715a40c8afbb94f38
F-Scienza <cufa1905@gmail.com>
2021-09-27 12:51:51 -0300

    terminada la verson mobile. hacer responsive para ipad y pc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const  contactButton = document.querySelector('#contact-button')
+let closeModal = document.querySelectorAll(".close")[0]
+let openModal = document.querySelectorAll('.cta')[0];
+let modal = document.querySelectorAll('.modal')[0];
+let modalC = document.querySelectorAll('.modal-container')[0];
+let gmailButton = document.querySelectorAll('#gmailButton')[0];
+let gmailShowed = document.querySelectorAll('#gmailDiv')[0]
+
+openModal.addEventListener('click', function (e) {
+	e.preventDefault();
+	modalC.style.opacity = '1';
+	modalC.style.visibility = 'visible';
+	modalC.classList.toggle('modal-close');
+});
+closeModal.addEventListener("click", function(){
+    modalC.classList.toggle("modal-close")
+	modalC.style.opacity = '0';
+	modalC.style.visibility = 'hidden';
+})
+gmailButton.addEventListener("click", function(){
+	gmailShowed.classList.toggle('hidden');
+	gmailShowed.style.visibility = 'visble';
+})
